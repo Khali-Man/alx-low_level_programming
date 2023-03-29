a286ef58e65dc14d58100151d329a2a0086f6970
Khal-Man <abeeblahidolapo@gmail.com>
2023-03-28 21:01:00 -0700

    Funtion that points to int as a prameter and update the balue to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Function that points to an int and update its point to 98
+ * @n:  Parameter that was iswd
+ * Return: *n
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
